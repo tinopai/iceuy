2ccbece58770cf160bed9d4b3a571aa000f27a25
tinopai <eltinopa@icloud.com>
2022-03-13 09:44:19 -0300

    fix: target='_blank' & removed description

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,18 +5,18 @@ export default function Index() {
           <div className="text-center">
               <div className="flex justify-center"><img src="/ice.png" /></div>
               <a className="text-white hover:text-blue-700 duration-300 text-7xl font-bold mb-4" href="https://ice.uy" target="_blank">ice.uy</a>
-              <p className="text-2xl text-gray-400 font-thin">it do be drippin doe</p>
+              {/* <p className="text-2xl text-gray-400 font-thin">it do be drippin doe</p> */}
               <div className="flex items-center justify-around mt-8">
                 <a href="https://github.com/tinopai" target="_blank">
                   <img className="linkIcon" src="/github.svg"/>
                 </a>
-                <a href="https://instagram.com/ttinoob">
+                <a href="https://instagram.com/ttinoob" target="_blank">
                   <img className="linkIcon" src="/instagram.svg"/>
                 </a>
-                <a href="https://open.spotify.com/user/b4rp9ca7dtbsfzsmre7cohytc">
+                <a href="https://open.spotify.com/user/b4rp9ca7dtbsfzsmre7cohytc" target="_blank">
                   <img className="linkIcon" src="/spotify.svg"/>
                 </a>
-                <a href="https://cpn.ac/users/tino">
+                <a href="https://cpn.ac/users/tino" target="_blank">
                   <img className="linkIcon" src="/cpn.svg"/>
                 </a>
               </div>
