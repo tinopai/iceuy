49a66c4e908377692e8c2098223384e04fb113f5
tinopai <eltinopa@icloud.com>
2022-11-02 14:07:57 -0300

    feat: added tiktok logo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -18,6 +18,23 @@ export default function Index() {
             <br />
             contact <kbd>iceiceice#2401</kbd> via Discord to purchase
           </p>
+          <div className="flex justify-center items-center gap-x-2 mt-4">
+            {
+              [
+                {
+                  name: 'TikTok',
+                  url: 'https://tiktok.com/@ice.uy',
+                  icon: '/tiktok.webp',
+                }
+              ].map((social: any, index:number) => {
+                return (<div key={index}>
+                  <a href={social.url} target="_blank">
+                    <img src={social.icon} className="h-8" style={{filter: "invert(100%)"}} />
+                  </a>
+                </div>)
+              })
+            }
+          </div>
         </div>
       </div>
     </div>
